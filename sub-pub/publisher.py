1bbf36473d67f9b5bd34de5bbf282610f5af831a
borysik <34368933+bvchirkov@users.noreply.github.com>
2023-03-22 16:20:11 +0400

    Fix

--- sub-pub/publisher.py
+++ sub-pub/publisher.py
@@ -1,8 +1,8 @@
 import rospy
 from std_msgs.msg import String
 
-rospy.init_node("udsu_ros__node_pub")
-pub = rospy.Publisher("udsu_ros__topic", String, queue_size=10)
+rospy.init_node("udsu_ros_node_pub")
+pub = rospy.Publisher("udsu_ros_topic", String, queue_size=10)
 
 r = rospy.Rate(10)
 s = String()
